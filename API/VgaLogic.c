d18aa8d68b23515af887faeed8fb37dd93ab3088
okke123 <okkevisser@gmail.com>
2019-05-01 11:58:20 +0200

    Bestand namen voor de API veranderd naar Vga omdat API te vaag was en Franc's code erin gezet die nu langzaam geconverteerd kan worden naar ons eigen formaat

---
+++ API/VgaLogic.c
@@ -0,0 +1,3 @@
+#include <VgaLogic.h>
+#include <VgaIO.h>
+
---
+++ Application/AppFront.c
@@ -0,0 +1,19 @@
+/*!
+ *	/arthur Chileam Bohnen
+ *	/version 0.1
+ *	/date 2019-4-30
+ *	/mainpage Front Layer
+ *		UART command handling
+ */
+
+#include "Demo_Front.h"
+
+int MessageToCommand(uint8_t* msg, msg_typedef* MSG_command)
+{
+	return 0x00;
+}
+
+void ErrorCodeHandler(uint8_t error, uint8_t* error_msg)
+{
+
+}
