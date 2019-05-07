a5f94948b6d8005929139325bbe234cc6891ef85
Chileam90 <chileam.bohnen@hotmail.com>
2019-05-07 23:25:27 +0200

    Doxgen documentatie

--- Application/AppFront.h
+++ Application/AppFront.h
@@ -1,6 +1,22 @@
 #ifndef __APP_FRONT_H
 #define __APP_FRONT_H
 
+/** @addtogroup AppFront
+ *  @{
+ *  	Error codes values:
+ * 		|	Error code	|	Error type									|
+ * 		|:-------------:|:----------------------------------------------|
+ * 		|		0x00	|	None error									|
+ * 		|		0x0n	|	Hardware related							|
+ * 		|		0x1n	|	Script commandos related					|
+ * 		|		0x2n	|	Draw functions related						|
+ * 		|		0x3n	|	Memory functions related					|
+ *
+ */
+
+/** @}*/
+
+
 void ErrorCodeHandler(int error, char* error_msg);
 
 #endif
